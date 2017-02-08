e9b478fd473454af6ed7d7c02e079e249abc2738
null <null@nullcraft.me>
2017-02-07 22:11:21 -0800

    Convert int to long

--- src/com/computerdude/epsilon/util/LevelUtil.java
+++ src/com/computerdude/epsilon/util/LevelUtil.java
@@ -7,20 +7,20 @@ import org.bukkit.entity.Player;
 public class LevelUtil {
 
     /** @return {@code base} to the power of {@code exp}, rounded to the nearest integer */
-    private static int pow(double base, double exp) {
+    private static long pow(double base, double exp) {
         return (int) Math.round(Math.pow(base, exp));
     }
 
     /** Return the amount of XP required to level up from level {@code (level)} to level {@code (level + 1)}. */
-    public static int getXP(int level) {
+    public static long getXP(int level) {
         if (level <= 10) return 100 * pow(1.3, level);
         if (level <= 25) return 1_379 * pow(1.2, level - 10);
         return 21_246 * pow(1.1, level - 25);
     }
 
     /** Return the amount of XP required to get from level 1 to level {@code level}. */
-    public static int getTotalXP(int level) {
-        return IntStream.range(1, level).map(LevelUtil::getXP).sum();
+    public static long getTotalXP(int level) {
+        return IntStream.range(1, level).mapToLong(LevelUtil::getXP).sum();
     }
 
     /**
@@ -29,7 +29,7 @@ public class LevelUtil {
      * @param level the player's current level.
      * @param xp the player's experience for the current level.
      */
-    public static void setXPBar(Player player, int level, int xp) {
+    public static void setXPBar(Player player, int level, long xp) {
         player.setLevel(level);
         player.setExp((float) xp / getXP(level));
     }
