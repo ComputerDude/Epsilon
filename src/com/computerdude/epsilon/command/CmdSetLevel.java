e903eabd829f3ec04e6c9c7b6c1963172abb27c0
null <null@nullcraft.me>
2017-02-07 18:30:02 -0800

    Add /setlevel ... [player] support

--- src/com/computerdude/epsilon/command/CmdSetLevel.java
+++ src/com/computerdude/epsilon/command/CmdSetLevel.java
@@ -1,6 +1,7 @@
 package com.computerdude.epsilon.command;
 
 import com.computerdude.epsilon.util.MySQL;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -20,19 +21,29 @@ public class CmdSetLevel implements CommandExecutor {
             return true;
         }
         Player player = (Player) sender;
-        if (args.length != 1) {
-            player.sendMessage(color("&4/setlevel <level>"));
-        } else {
-            final int amount;
-            try {
-                amount = Integer.parseInt(args[0]);
-            } catch (NumberFormatException e) {
-                sender.sendMessage(color("&4That is not a number!"));
+        if (args.length == 0 || args.length > 2) {
+            player.sendMessage(color("&4/setlevel <level> [player]"));
+            return true;
+        }
+        final int amount;
+        final Player target;
+        try {
+            amount = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            sender.sendMessage(color("&4That is not a number!"));
+            return true;
+        }
+        if (args.length >= 2) {
+            target = Bukkit.getPlayer(args[1]);
+            if (target == null) {
+                sender.sendMessage(color("&4Player not found!"));
                 return true;
             }
-            MySQL.setLevel(player, amount);
-            player.sendMessage(colorf("&aChanged level to %s.", args[0]));
+        } else {
+            target = player;
         }
+        MySQL.setLevel(target, amount);
+        player.sendMessage(colorf("&aChanged %s's level to %s.", target.getName(), args[0]));
         return true;
     }
 }
