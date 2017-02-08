2f4067d074934818f2a1e3aed9b34cfa4183f38e
null <null@nullcraft.me>
2017-02-08 12:46:49 -0800

    Create CmdSetCoins.java

---
+++ src/com/computerdude/epsilon/command/CmdSetCoins.java
@@ -0,0 +1,20 @@
+package com.computerdude.epsilon.command;
+
+import com.computerdude.epsilon.util.MySQL;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import static com.computerdude.epsilon.util.ColorUtil.color;
+import static com.computerdude.epsilon.util.ColorUtil.colorf;
+
+public class CmdSetCoins implements CommandExecutor {
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        //TODO
+        return false;
+    }
+    
+}
