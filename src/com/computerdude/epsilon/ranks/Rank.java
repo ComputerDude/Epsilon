1d03193f38e9288f337e9b921da0d6adfc33a330
ComputerDude <playbaseball@gmail.com>
2017-02-10 20:03:02 -0500

    Edited "Rank.java"

--- src/com/computerdude/epsilon/ranks/Rank.java
+++ src/com/computerdude/epsilon/ranks/Rank.java
@@ -2,7 +2,9 @@ package com.computerdude.epsilon.ranks;
 
 public enum Rank {
 
-	DEFAULT(0), ADMIN(5000), OWNER(9001);
+	//Staff ranks have a 100 Gap. If we ever add a rank between others, add 50, and 25 after that.
+	
+	DEFAULT(0), TRIAL(8500), MODERATOR(8600), ART(8700), DEV(8800), ADMIN(8900), OWNER(9001);
 
 	private final int level;
 
