78c5a701419721bd4a2eb218e14fa06bb04dd512
null <null@nullcraft.me>
2017-02-11 12:42:11 -0800

    Improve EPlayer, a lot

--- src/com/epsilon/player/EPlayer.java
+++ src/com/epsilon/player/EPlayer.java
@@ -1,5 +1,7 @@
 package com.epsilon.player;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.UUID;
 
 import com.epsilon.ranks.Rank;
@@ -10,12 +12,26 @@ import org.bukkit.entity.Player;
 
 public class EPlayer {
 
-    private final OfflinePlayer player;
-    private UUID uuid;
+    private static final List<EPlayer> PLAYERS = new LinkedList<>();
 
-    public EPlayer(Player player) {
+    public static EPlayer getPlayer(OfflinePlayer player) {
+        for (EPlayer ep : PLAYERS) {
+            // TODO replace with binary search
+            if (ep.getUniqueId().equals(player.getUniqueId())) return ep;
+        }
+        final EPlayer ep = new EPlayer(player);
+        PLAYERS.add(ep);
+        return ep;
+    }
+
+    private final OfflinePlayer player;
+
+    /**
+     * @deprecated This constructor should be private--other classes, use {@link #getPlayer()}
+     */
+    @Deprecated
+    public EPlayer(OfflinePlayer player) {
         this.player = player;
-        uuid = player.getUniqueId();
     }
 
     /**
@@ -26,8 +42,11 @@ public class EPlayer {
         return new EPlayer(player).getRank();
     }
 
+    /**
+     * <b>Queries MySQL, please run asynchronously.</b>
+     */
     public Rank getRank() {
-        Integer rank = MySQL.getProperty(uuid, "rank");
+        Integer rank = MySQL.getProperty(player.getUniqueId(), "rank");
         if (rank == null) {
             return Rank.DEFAULT;
         } else {
@@ -39,8 +58,29 @@ public class EPlayer {
                         "contact an administrator."));
             return Rank.DEFAULT;
         }
+    }
 
+    public OfflinePlayer getPlayer() {
+        return player;
+    }
 
+    public Player getOnlinePlayer() {
+        if (!player.isOnline()) throw new IllegalStateException("Player is not online");
+        return (Player) player;
+    }
+
+    public UUID getUniqueId() {
+        return player.getUniqueId();
+    }
+
+    @Override
+    public boolean equals(Object that) {
+        return that instanceof EPlayer && this.getUniqueId().equals(((EPlayer) that).getUniqueId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getUniqueId().hashCode();
     }
 
 }
