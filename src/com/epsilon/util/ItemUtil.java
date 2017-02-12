d3a2656aaa1ac1a3cc44912586da0948b3b751a5
ComputerDude <playbaseball@gmail.com>
2017-02-12 17:46:08 -0500

    Added an "addGlow" method to ItemUtil

--- src/com/epsilon/util/ItemUtil.java
+++ src/com/epsilon/util/ItemUtil.java
@@ -3,8 +3,12 @@ package com.epsilon.util;
 import java.util.Arrays;
 
 import org.bukkit.Material;
+import org.bukkit.craftbukkit.v1_11_R1.inventory.CraftItemStack;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+
+import net.minecraft.server.v1_11_R1.NBTTagCompound;
+import net.minecraft.server.v1_11_R1.NBTTagList;
 
 public class ItemUtil {
 
@@ -24,5 +28,21 @@ public class ItemUtil {
         item.setItemMeta(meta);
         return item;
     }
+    
+    //Use ItemStack itm = ItemUtil.addGlow(itm); not just ItemUtil.addGlow(itm);
+    public static ItemStack addGlow(ItemStack item) {
+		net.minecraft.server.v1_11_R1.ItemStack nmsStack = CraftItemStack.asNMSCopy(item);
+		NBTTagCompound tag = null;
+		if (!nmsStack.hasTag()) {
+			tag = new NBTTagCompound();
+			nmsStack.setTag(tag);
+		}
+		if (tag == null)
+			tag = nmsStack.getTag();
+		NBTTagList ench = new NBTTagList();
+		tag.set("ench", ench);
+		nmsStack.setTag(tag);
+		return CraftItemStack.asCraftMirror(nmsStack);
+	}
 
 }
