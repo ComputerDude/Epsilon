e2d8079e56b9f03f661eeaa1c9d55afd527877ed
ComputerDude <playbaseball@gmail.com>
2017-02-11 23:26:28 -0500

    Fixed Bugs with Adminmode

--- src/com/epsilon/administration/AdminMode.java
+++ src/com/epsilon/administration/AdminMode.java
@@ -10,16 +10,23 @@ public class AdminMode {
 	public static void toggleAdminMode(Player player) {
 
 		if (Epsilon.adminModeConfig.getBoolean("player." + player.getUniqueId() + ".adminMode")) {
-			player.getInventory().setContents((ItemStack[]) Epsilon.adminModeConfig.get("player." + player.getUniqueId() + ".adminModeInv"));
+			if (Epsilon.adminModeConfig.get("player." + player.getUniqueId() + ".adminModeInv") != null) {
+				player.getInventory().setContents(
+						(ItemStack[]) Epsilon.adminModeConfig.get("player." + player.getUniqueId() + ".adminModeInv"));
+			} else {
+				player.getInventory().clear();
+			}
 			Epsilon.adminModeConfig.set("player." + player.getUniqueId() + ".adminMode", false);
 			Epsilon.adminModeConfig.set("player." + player.getUniqueId() + ".adminModeInv", null);
 			Epsilon.adminModeConfig.save();
 			Epsilon.adminModeConfig.reload();
 		} else {
-			Epsilon.adminModeConfig.set("player." + player.getUniqueId() + ".adminModeInv",	player.getInventory().getContents());
-			
-			player.getInventory().clear(); // Use #clear() method instead. - JustBru00
-			
+			Epsilon.adminModeConfig.set("player." + player.getUniqueId() + ".adminModeInv",
+					player.getInventory().getContents());
+
+			player.getInventory().clear(); // Use #clear() method instead. -
+											// JustBru00
+
 			Epsilon.adminModeConfig.set("player." + player.getUniqueId() + ".adminMode", true);
 			Epsilon.adminModeConfig.save();
 			Epsilon.adminModeConfig.reload();
