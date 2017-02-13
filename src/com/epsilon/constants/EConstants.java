b7802c670e49ee2ed148fb439e3046bdad874a00
Justin Brubaker <justbru00@gmail.com>
2017-02-12 21:09:47 -0500

    Added EConstants class
    Changed Join and respawn

---
+++ src/com/epsilon/constants/EConstants.java
@@ -0,0 +1,10 @@
+package com.epsilon.constants;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+
+public class EConstants {
+
+	public static final Location SPAWN = new Location(Bukkit.getWorld("world"), 0, 90, 0);
+	
+}
--- src/com/epsilon/listeners/OnJoinAndRespawn.java
+++ src/com/epsilon/listeners/OnJoinAndRespawn.java
@@ -2,7 +2,6 @@ package com.epsilon.listeners;
 
 import org.apache.commons.lang3.StringUtils;
 import org.bukkit.Bukkit;
-import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,14 +9,13 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.scoreboard.Team;
 
+import com.epsilon.constants.EConstants;
 import com.epsilon.player.EPlayer;
 import com.epsilon.util.ColorUtil;
 import com.epsilon.util.LevelUtil;
 import com.epsilon.util.MySQL;
 
-public class OnJoinAndRespawn implements Listener {
-
-    public static final int[] SPAWN = {0, 90, 0};
+public class OnJoinAndRespawn implements Listener {   
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent e) {
@@ -30,16 +28,14 @@ public class OnJoinAndRespawn implements Listener {
         t.addEntry(player.getName());
         
         LevelUtil.setXPBar(player, MySQL.getLevel(player), MySQL.getProperty(player, "exp"));
-        if (!player.hasPlayedBefore()) {
-            Location loc = new Location(e.getPlayer().getWorld(), SPAWN[0], SPAWN[1], SPAWN[2]);
-            player.teleport(loc);
+        if (!player.hasPlayedBefore()) {           
+            player.teleport(EConstants.SPAWN);
         }
     }
     
     @EventHandler
-    public void onPlayerRespawn(PlayerRespawnEvent e) {
-    	Location loc = new Location(e.getPlayer().getWorld(), SPAWN[0], SPAWN[1], SPAWN[2]);
-    	e.getPlayer().teleport(loc);
+    public void onPlayerRespawn(PlayerRespawnEvent e) {    
+    	e.getPlayer().teleport(EConstants.SPAWN);
     }
 
 }
