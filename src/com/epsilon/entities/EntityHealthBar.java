a1586a74da452b638e51bb5d6bdc1691f30df1c2
Justin Brubaker <justbru00@gmail.com>
2017-02-12 22:11:43 -0500

    Added formatBar

--- src/com/epsilon/entities/EntityHealthBar.java
+++ src/com/epsilon/entities/EntityHealthBar.java
@@ -2,4 +2,42 @@ package com.epsilon.entities;
 
 public class EntityHealthBar {
 
+	/**
+	 * For formating the health bar.
+	 * @param health The health of the entity
+	 * @param maxHealth The max health of the entity.
+	 * @author JustBru00
+	 */
+	private String formatBar(int health, int maxHealth) { // Design is: "[:::::healthLeft:::::]"
+		int percent = (health / maxHealth) * 100;
+		
+		if (percent >= 90) {
+			return "&8[&a:::::&c" + health + "&a:::::&8]";
+		} else if (percent >= 80 && percent <= 90) {
+			return "&8[&a:::::&c" + health + "&a::::&c:&8]";
+		} else if (percent >= 70 && percent <= 80) {
+			return "&8[&a:::::&c" + health + "&a:::&c::&8]";
+		} else if (percent >= 60 && percent <= 70) {
+			return "&8[&a:::::&c" + health + "&a::&c:::&8]";
+		} else if (percent >= 50 && percent <= 60) {
+			return "&8[&a:::::&c" + health + "&a:&c::::&8]";
+		} else if (percent >= 40 && percent <= 50) {
+			return "&8[&a:::::&c" + health + "&c:::::&8]";
+		} else if (percent >= 30 && percent <= 40) {
+			return "&8[&a::::&c:" + health + "&c:::::&8]";
+		} else if (percent >= 20 && percent <= 30) {
+			return "&8[&a:::&c::" + health + "&c:::::&8]";
+		} else if (percent >= 10 && percent <= 20) {
+			return "&8[&a::&c:::" + health + "&c:::::&8]";
+		} else if (percent > 0 && percent <= 10) {
+			return "&8[&a:&c::::" + health + "&c:::::&8]";
+		} else if (percent == 0) {
+			return "&8[&c:::::" + health + "&c:::::&8]";
+		} else if (percent < 0) {
+			return "&cThis really should be dead.";
+		} else {
+			return "&cSomeone messed up";
+		}
+	}
+	
 }
