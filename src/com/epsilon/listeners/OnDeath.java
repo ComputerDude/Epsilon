bfb027232d16855e92e9f95c4658ff401fda1801
Justin Brubaker <justbru00@gmail.com>
2017-02-12 21:20:27 -0500

    Added respawning
    Change it if you want.

--- src/com/epsilon/listeners/OnDeath.java
+++ src/com/epsilon/listeners/OnDeath.java
@@ -1,8 +1,12 @@
 package com.epsilon.listeners;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+
+import com.epsilon.Epsilon;
 
 /**
  * Class for death handling
@@ -13,7 +17,18 @@ public class OnDeath implements Listener {
 
 	@EventHandler
 	public void onDeath(PlayerDeathEvent e) {
-		// Stuff (Maybe?)
+		
+		final Player p = e.getEntity();
+		
+		// Respawn player after 3 ticks (One or two ticks will not work sometimes.)
+		Bukkit.getScheduler().scheduleSyncDelayedTask(Epsilon.getInstance(), new Runnable() {
+			
+			@Override
+			public void run() {
+				p.spigot().respawn(); 				
+			}
+		}, 3);
+		
 	}
 	
 }
