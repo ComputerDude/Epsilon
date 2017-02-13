228567cea624cb026bef8ea8323487af00afeed2
null <null@nullcraft.me>
2017-02-13 15:34:18 -0800

    Move world name to a constant

--- src/com/epsilon/util/Const.java
+++ src/com/epsilon/util/Const.java
@@ -5,6 +5,7 @@ import org.bukkit.Location;
 
 public class Const {
 
-	public static final Location SPAWN = new Location(Bukkit.getWorld("world"), 0, 90, 0);
+	public static final String WORLD_NAME = "world";
+	public static final Location SPAWN = new Location(Bukkit.getWorld(WORLD_NAME), 0, 90, 0);
 	
 }
