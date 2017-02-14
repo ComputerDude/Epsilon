a40db3ddf288734625ce20925dfe9d3d6f832570
ComputerDude <playbaseball@gmail.com>
2017-02-13 20:57:06 -0500

    Added Constructor and some Objects to ArmorPiece.java

--- src/com/epsilon/item/armor/ArmorPiece.java
+++ src/com/epsilon/item/armor/ArmorPiece.java
@@ -1,5 +1,7 @@
 package com.epsilon.item.armor;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 import org.bukkit.Material;
 
 /**
@@ -8,9 +10,27 @@ import org.bukkit.Material;
 public abstract class ArmorPiece {
 
 	private static Material armor;
-	
+	private static int durability;
+
 	protected ArmorPiece(Material armor) {
 		this.armor = armor;
+
+		if (armor.name().contains("DIAMOND")) {
+			this.durability = ThreadLocalRandom.current().nextInt(750, 2250);
+
+		} else if (armor.name().contains("IRON")) {
+			this.durability = ThreadLocalRandom.current().nextInt(500, 1500);
+
+		} else if (armor.name().contains("CHAINMAIL")) {
+			this.durability = ThreadLocalRandom.current().nextInt(250, 750);
+
+		} else if (armor.name().contains("GOLD")) {
+			this.durability = ThreadLocalRandom.current().nextInt(125, 375);
+
+		} else if (armor.name().contains("LEATHER")) {
+			this.durability = ThreadLocalRandom.current().nextInt(50, 150);
+
+		}
 	}
-	
+
 }
