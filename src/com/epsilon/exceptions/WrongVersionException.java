deb09007081cfeecd9ce1e8e9a76dd3048aa61b6
null <null@nullcraft.me>
2017-02-14 21:16:13 -0800

    Make WrongVersionException deprecated

--- src/com/epsilon/exceptions/WrongVersionException.java
+++ src/com/epsilon/exceptions/WrongVersionException.java
@@ -1,5 +1,9 @@
 package com.epsilon.exceptions;
 
+/**
+ * @deprecated This isn't really necessary
+ */
+@Deprecated
 public class WrongVersionException extends Exception {
 
 	private static final long serialVersionUID = 637783424848621421L;
