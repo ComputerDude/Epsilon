a5b733d2a06673ebdf87a1c2170f1e134774df2a
null <null@nullcraft.me>
2017-02-14 19:28:08 -0800

    *Completely* finish fixing items

--- src/com/epsilon/item/weapon/Sword.java
+++ src/com/epsilon/item/weapon/Sword.java
@@ -1,41 +1,38 @@
 package com.epsilon.item.weapon;
 
-import static com.epsilon.util.RandomUtil.randIntTriangular;
-
-import java.util.Map;
-
+import com.epsilon.item.EItem;
+import com.epsilon.item.RequirementType;
 import org.bukkit.Material;
 
-public abstract class Sword {
+public class Sword extends EItem {
 
-	public abstract Material getMaterial();
-	
-	private int durability;
-	private int damage;
-	private int[] minLevel = {1, 30, 60, 90, 120};
-	
-	protected Sword() {
-		
-		if (getMaterial().name().startsWith("DIAMOND")) {
-			this.durability = randIntTriangular(750, 2251);
-		} else if (getMaterial().name().startsWith("IRON")) {
-			this.durability = randIntTriangular(500, 1501);
-		} else if (getMaterial().name().startsWith("STONE")) {
-			this.durability = randIntTriangular(250, 751);
-		} else if (getMaterial().name().startsWith("GOLD")) {
-			this.durability = randIntTriangular(125, 376);
-		} else if (getMaterial().name().startsWith("WOOD")) {
-			this.durability = randIntTriangular(50, 151);
-		}
-		
-	}
-	
-	public int getDurability() {
-		return durability;
-	}
-	
-	public int getDamage() {
-		return damage;
-	}
-	
+    private static final Sword[] ITEMS = new Sword[5];
+
+    static {
+        ITEMS[0] = new Sword("Wooden Sword", Material.WOOD_SWORD, 100, 4);
+        ITEMS[0].setRequirement(RequirementType.LEVEL, 1);
+        ITEMS[1] = new Sword("Stone Sword", Material.STONE_SWORD, 200, 9);
+        ITEMS[1].setRequirement(RequirementType.LEVEL, 30);
+        ITEMS[2] = new Sword("Gold Sword", Material.GOLD_SWORD, 400, 16);
+        ITEMS[2].setRequirement(RequirementType.LEVEL, 60);
+        ITEMS[3] = new Sword("Iron Sword", Material.IRON_SWORD, 800, 25);
+        ITEMS[3].setRequirement(RequirementType.LEVEL, 90);
+        ITEMS[4] = new Sword("Diamond Sword", Material.DIAMOND_SWORD, 1600, 36);
+        ITEMS[4].setRequirement(RequirementType.LEVEL, 120);
+    }
+
+    /**
+     * Get the highest levelled sword whose level is less than or equal to the argument.
+     */
+    public static Sword getSword(int level) {
+        for (int i = ITEMS.length - 1; i >= 0; i--) {
+            if (((Integer) ITEMS[i].getRequirement(RequirementType.LEVEL)) <= level) return ITEMS[i];
+        }
+        return null;
+    }
+    
+    public Sword(String name, Material material, int maxDurability, int buffQuality) {
+        super(name, material, maxDurability, buffQuality);
+    }
+
 }
