0ec61b4df916f7b8088c69c4b12aa5e886fc175c
null <null@nullcraft.me>
2017-03-18 18:11:06 -0700

    Improve on{Enable,Disable}()

--- src/com/epsilon/Epsilon.java
+++ src/com/epsilon/Epsilon.java
@@ -1,11 +1,5 @@
 package com.epsilon;
 
-import com.epsilon.util.I18n;
-import org.bukkit.Bukkit;
-import org.bukkit.GameMode;
-import org.bukkit.plugin.PluginManager;
-import org.bukkit.plugin.java.JavaPlugin;
-
 import com.epsilon.chat.PlayerChat;
 import com.epsilon.command.CmdAdminMode;
 import com.epsilon.command.CmdGameMode;
@@ -19,11 +13,17 @@ import com.epsilon.listeners.OnEntityDamage;
 import com.epsilon.listeners.OnInteractEntity;
 import com.epsilon.listeners.OnJoinAndRespawn;
 import com.epsilon.listeners.OnPrePlayerLogin;
+import com.epsilon.util.I18n;
 import com.epsilon.util.MySQL;
 import com.epsilon.util.PluginFile;
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
 
 /**
- * Main class of the plugin. (Extends {@link JavaPlugin}) 
+ * Main class of the plugin. (Extends {@link JavaPlugin})
+ *
  * @author Multible Authors
  */
 public class Epsilon extends JavaPlugin {
@@ -34,19 +34,20 @@ public class Epsilon extends JavaPlugin {
 
     @Override
     public void onEnable() {
-    	instance = this;
+        long startTime = System.nanoTime();
+        instance = this;
         storageFile = new PluginFile(this, "storage.yml", "storage.yml");
         adminModeConfig = new PluginFile(this, "admin-mode.yml");
         I18n.setConfig(new PluginFile(this, "i18n.yml", "i18n.yml"));
+        getLogger().info("Registering listeners...");
         final PluginManager pm = Bukkit.getServer().getPluginManager();
-        
         pm.registerEvents(new OnJoinAndRespawn(), this);
         pm.registerEvents(new PlayerChat(), this);
         pm.registerEvents(new OnPrePlayerLogin(), this);
         pm.registerEvents(new OnDeath(), this);
         pm.registerEvents(new OnInteractEntity(), this);
         pm.registerEvents(new OnEntityDamage(), this);
-
+        getLogger().info("Registering commands...");
         getCommand("setlevel").setExecutor(new CmdSetLevel());
         getCommand("getlevel").setExecutor(new CmdGetLevel());
         getCommand("setcoins").setExecutor(new CmdSetCoins());
@@ -57,18 +58,21 @@ public class Epsilon extends JavaPlugin {
         getCommand("gms").setExecutor(new CmdGameMode(GameMode.SURVIVAL));
         getCommand("gma").setExecutor(new CmdGameMode(GameMode.ADVENTURE));
         getCommand("gmsp").setExecutor(new CmdGameMode(GameMode.SPECTATOR));
-        
+        getLogger().info("Initializing MySQL...");
         MySQL.createTables();
-        getLogger().info("Epsilon has been enabled.");
+        getLogger().info(String.format("Epsilon has been enabled in %,.6fs.", (System.nanoTime() - startTime) / 1e9));
     }
 
     @Override
     public void onDisable() {
-        MySQL.disconnect();
+        long startTime = System.nanoTime();
         instance = null;
+        getLogger().info("Disconnecting MySQL...");
+        MySQL.disconnect();
+        getLogger().info(String.format("Epsilon has been disabled in %,.6fs.", (System.nanoTime() - startTime) / 1e9));
     }
-    
+
     public static Epsilon getInstance() {
-    	return instance;
+        return instance;
     }
 }
